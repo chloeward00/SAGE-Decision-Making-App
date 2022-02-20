d70c7d41d35d56106fe6fd91d618eda9d81c35ab
Chloe <chloe.ward35@mail.dcu.ie>
2022-02-20 19:47:26 +0000

    Added swipe footer and header

---
+++ src/sage-project/pages/Cards/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+//import PersonIcon from "@material-ui/icons/Person";
+//import IconButton from "@material-ui/core/IconButton";
+
+export default function Header() {
+  return (
+    <div className="header">
+      
+      
+    </div>
+  );
+}
