16e4d5a539597d632c5ed52c9b247c28554c7de6
Chloe <chloe.ward35@mail.dcu.ie>
2022-03-05 16:13:57 +0000

    Updated file names

--- src/sage-project/components/Cards/Footer.js
+++ src/sage-project/components/Cards/Footer.js
@@ -12,7 +12,7 @@ export default function Footer() {
         <FavoriteIcon
         className="fav"
         onClick={() => {
-          Router.push("/Cards/Fav");
+          Router.push("/cards/fav");
          
         }}
       >
