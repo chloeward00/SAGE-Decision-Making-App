f2b16954e0b051ed20954fb6551026483e4f7d12
talvoj2 <joanna.talvo2@mail.dcu.ie>
2022-03-10 21:44:53 +0000

    fixing UI design

--- src/sage-project/src/theme.js
+++ src/sage-project/src/theme.js
@@ -3,17 +3,23 @@ import { red } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#556cd6',
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
-    },
-  },
+    stylesBG: {
+        blue: '#96E9EE',
+        purple: '#9896EE',
+        pink: '#D796EE',
+        rose: '#EE96B5'
+    }
 });
 
 export default theme;
