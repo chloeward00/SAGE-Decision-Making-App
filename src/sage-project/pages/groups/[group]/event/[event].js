43ce1403eecaf993a6387d80c68b2a11add435ca
talvoj2 <joanna.talvo2@mail.dcu.ie>
2022-04-14 18:49:16 +0100

    fixed the Comments UI, moved it to the events instead of the groups, updated the database

--- src/sage-project/pages/groups/[group]/event/[event].js
+++ src/sage-project/pages/groups/[group]/event/[event].js
@@ -1,9 +1,5 @@
 
-import Groups from "../../../../components/Dashboard/Groups/GroupsList";
-import GroupsBanner from "../../../../components/Dashboard/Groups/GroupsBanner";
-import Layout from "../../../../components/Layout/Layout";
 import PageLayout from "../../../../components/Layout/PageLayout";
-import IndividualEventCard  from "../../../../components/Dashboard/Events/IndividualEventCard"
 import { useState, useEffect } from 'react';
 import fire from 'firebase/app'
 import 'firebase/firestore';
@@ -11,16 +7,11 @@ import 'firebase/auth'
 import { useRouter } from "next/router";
 import IndividualEvent from "../../../../components/Dashboard/Events/IndividualEvent";
 import EventsBanner from "../../../../components/Dashboard/Events/EventsBanner";
-
-
-// const groupName = "Groups";
-// const buttonTitle = "Create a new group";
-// const groupID = "";
+import Comments from "../../../../components/Dashboard/Comments/Comments";
 
 const EventPage = () => {
 
     const router = useRouter();
-
     const url = router.asPath.split('/')
     const urlCategory = url[2]
   
@@ -34,7 +25,7 @@ const EventPage = () => {
     const [individualEventData, setIndividualEventData] = useState([])
     
     useEffect(() => {
-    // isMounted is added to prevent memory leaks
+        // isMounted is added to prevent memory leaks
 
         let isMounted = true;
 
@@ -51,8 +42,6 @@ const EventPage = () => {
                     setEventName(querySnapshot.data().eventName)
                     setIndividualEventData(querySnapshot.data())
                 }
-                // console.log("print indi event heree  " + querySnapshot.data().eventID)
-                // setEventName(querySnapshot.data().eventName)
             });
         }
 
@@ -68,8 +57,8 @@ const EventPage = () => {
         <div>
             <PageLayout>
                 <EventsBanner eventName={eventName} groupID={groupID} eventID={eventID} eventDetails={individualEventData} />
-                <h1>PUT THE EVENT DATA HERE AND LET THE MEMBERS DO THE SURVEY AND LET THE ADMIN EDIT STUFF</h1>
                 <IndividualEvent eventName={eventName} eventID={eventID} groupID={groupID} eventDetails={individualEventData}/>
+                <Comments/>
             </PageLayout>
         </div>
     );
